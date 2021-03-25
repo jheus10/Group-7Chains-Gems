a4780c3e67c54180be9794787027150b09e6d54c
jheus10 <56898851+jheus10@users.noreply.github.com>
2021-03-25 22:54:07 +0800

    Add files via upload

---
+++ connections/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+    function connection (){
+
+        $host = "localhost";
+        $username = "root";
+        $password ="";
+        $database = "db_ecommerce";
+
+        $con = new mysqli($host, $username, $password, $database);
+
+        if ($con->connect_error ){
+            echo $con->connect_error;
+        }
+        
+        else {
+            return $con;
+        }
+        
+    }
